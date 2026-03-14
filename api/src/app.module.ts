60694a1dea359e02a53da4164b0ae139b75f70c8
steevnprm <steevn.pyram@gmail.com>
2026-03-14 10:11:24 +0100

    chore: remove auth and user components

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    UsersModule, 
-    AuthModule
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
